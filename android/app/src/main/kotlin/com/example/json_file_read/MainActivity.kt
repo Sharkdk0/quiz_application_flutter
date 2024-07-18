9a311989f802d1264c2f3f4fb26697fb2f09a050
shar-kdk <sharvikhadka@gmail.com>
2024-07-18 12:17:47 +0545

    first commit

---
+++ android/app/src/main/kotlin/com/example/json_file_read/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.json_file_read
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,170 @@
+import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: QuizHomePage(),
+      theme: ThemeData(
+        textTheme: GoogleFonts.latoTextTheme(),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+                foregroundColor: Colors.white,
+                backgroundColor: Colors.blue,
+                shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                ),
+                fixedSize: Size(200, 50))),
+      ),
+    );
+  }
+}
+
+Future<List<dynamic>> loadJson() async {
+  String jsonString = await rootBundle.loadString('assets/data.json');
+  return json.decode(jsonString)['quiz'];
+}
+
+class QuizHomePage extends StatefulWidget {
+  const QuizHomePage({super.key});
+
+  @override
+  State<QuizHomePage> createState() => _QuizHomePageState();
+}
+
+class _QuizHomePageState extends State<QuizHomePage> {
+  List<dynamic>? _questionsList;
+  int _currentQuestionList = 0;
+  int _score = 0;
+  bool _quizFinished = false;
+  bool _startQuiz = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadQuestions();
+  }
+
+  Future<void> _loadQuestions() async {
+    List<dynamic> questions = await loadJson();
+    setState(() {
+      _questionsList = questions;
+    });
+  }
+
+  void _answerQuestion(String answer) {
+    if (answer == _questionsList![_currentQuestionList]['answer']) {
+      _score++;
+    }
+    setState(() {
+      if (_currentQuestionList < _questionsList!.length - 1) {
+        _currentQuestionList++;
+      } else {
+        _quizFinished = true;
+      }
+    });
+  }
+
+  void _startQuizBtn() {
+    setState(() {
+      _startQuiz = true;
+    });
+  }
+
+  void _restartQuiz() {
+    setState(() {
+      _currentQuestionList = 0;
+      _score = 0;
+      _quizFinished = false;
+      _startQuiz = false;
+    });
+  }
+
+  Widget _buildOptionButton(String option) {
+    return ElevatedButton(
+      onPressed: () {
+        _answerQuestion(option);
+      },
+      child: Text(option),
+    );
+  }
+
+  List<Widget> _buildOptions() {
+    List<Widget> options = [];
+    for (String option in _questionsList![_currentQuestionList]['options']) {
+      options.add(_buildOptionButton(option));
+      options.add(SizedBox(height: 10));
+    }
+    return options;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (!_startQuiz) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text("Quiz App"),
+          centerTitle: true,
+          backgroundColor: Colors.deepPurple,
+          elevation: 10,
+        ),
+        body: Center(
+          child: ElevatedButton(
+            onPressed: _startQuizBtn,
+            child: Text('Start Quiz'),
+          ),
+        ),
+      );
+    } else if (_quizFinished) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text("Quiz App"),
+          centerTitle: true,
+          backgroundColor: Colors.deepPurple,
+          elevation: 10,
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              Text('Your score: $_score / ${_questionsList!.length}'),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: _restartQuiz,
+                child: Text('Restart Quiz'),
+              ),
+            ],
+          ),
+        ),
+      );
+    } else {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text("Quiz App"),
+          centerTitle: true,
+          backgroundColor: Colors.deepPurple,
+          elevation: 10,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Text(_questionsList![_currentQuestionList]['question']),
+              SizedBox(height: 20),
+              ..._buildOptions(),
+            ],
+          ),
+        ),
+      );
+    }
+  }
+}
